e65e545f5db2264cdfb5299f5e7f0bb64397a63f
anton-git <anton-87@ya.ru>
2016-03-19 23:45:01 +0300

    project initializing

---
+++ README.md
@@ -0,0 +1,5 @@
+Project for development training.
+
+At first it should implement simple ui imulating ATM working.
+
+Later Server Side can be added which client talks to using REST.
---
+++ js/scripts.js
@@ -0,0 +1,104 @@
+var app = angular.module('atm-app', ['ngRoute']);
+
+app
+
+.config(['$routeProvider', function($routeProvider) {
+    
+    $routeProvider
+      
+      .when('/enter', {
+        templateUrl: 'partials/enter.html',
+        controller: 'ViewEnterScreenController'
+      })
+
+      .when('/main', {
+        templateUrl: 'partials/main.html',
+        controller: 'ViewInformationScreenController'
+      })
+
+      .when('/cash', {
+        templateUrl: 'partials/cash.html',
+        controller: 'ViewEnterScreenController'
+      })
+
+      .otherwise({
+        redirectTo: '/enter'
+      });
+}])
+
+.controller('ViewEnterScreenController', ['$scope', '$location', 'Account', 
+    function($scope, $location, Account) {
+
+
+    $scope.cardNumber;
+    $scope.pinCode;
+
+    $scope.enter = function(cardNumber, pinCode) {
+
+        if (Account.validate(cardNumber, pinCode)) {
+            $location.path("/main");
+        } else {
+            alert('Validation Failed!');
+        }
+
+    };
+
+}])
+
+.controller('ViewInformationScreenController', ['$scope', '$location', 'Account', 
+    function($scope, $location, Account) {
+
+    $scope.name = Account.getName();
+    $scope.money = Account.getAmount();
+
+    $scope.getMoney = function(){
+        $location.path("/cash");
+    };
+
+    $scope.back = function(){
+        $location.path("/enter");
+    };
+
+
+}])
+
+.filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  }
+
+})
+
+.factory('Account', function() {
+    
+    var user = {    
+        'id'        : 1,
+        'name'      : 'mr Smith', 
+        'cardNumber': '2000-0001',
+        'pinCode'   : '313',
+        'amount'    : 1000
+    };
+
+    return {
+
+        validate: function(cardNumber, pinCode) {
+            console.log('cardNumber = ' + cardNumber);
+            console.log('pinCode = ' + pinCode);
+            return user.cardNumber === cardNumber && user.pinCode === pinCode;
+        },
+
+        getName: function() {
+            return angular.copy(user.name);
+        },
+
+        getMoney: function(amount) {
+            user.amount = user.amount - amount;
+        },
+
+        getAmount: function() {
+            return angular.copy(user.amount);
+        }
+    }
+    
+})
+;
